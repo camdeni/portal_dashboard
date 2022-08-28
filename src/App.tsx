72bbee7252087af38f514e0598a506419db5ae48
gratien <gracian2020@gmail.com>
2022-08-28 10:04:10 +0200

    removed unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from "react";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-
-function App() {
+const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-
+      <div>Welcome To Home Page</div>
     </QueryClientProvider>
   );
-}
+};
 
 export default App;
